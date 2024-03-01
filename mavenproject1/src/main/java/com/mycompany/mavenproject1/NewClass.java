b6ae13dcc172aac6ede7adfd89076fb7c51046db
Tecno Espectro <TecnoEspectro15@hotmail.com>
2024-03-01 11:33:47 -0500

    Creacion de clase interna

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -0,0 +1,41 @@
+package com.mycompany.mavenproject1;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SCIS2PC13
+ */
+public class NewClass {
+    public static void main(String[] args){
+        Character homero = new Character("Homero","Inspector de seguridad", 33, "A la grande le puse cuca");
+        homero.greeting();
+        System.out.println(homero.age);
+        
+        Character bart = new Character("Bartolomeo","Vago", 10, "Ay caramba");
+        bart.greeting();
+}
+    static class Character{
+    String name;
+    String occupation;
+    int age;
+    String phrase;
+
+        public Character(String name, String occupation, int age, String phrase) {
+            this.name = name;
+            this.occupation = occupation;
+            this.age = age;
+            this.phrase = phrase;
+        }
+    
+    
+    
+    void greeting(){
+        System.out.println(this.phrase);
+    }
+    }
+    
+}
